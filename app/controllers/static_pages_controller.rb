f8afcd0bd00249d10ab91aab1be30f1adf6544c8
phbord <phbord@gmail.com>
2021-06-14 13:07:17 +0200

    create  static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def contact
+  end
+
+  def team
+  end
+
+  def privacy_policy
+  end
+
+  def cgv
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # static pages
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/team', to: 'static_pages#team', via: 'get'
+  match '/cgv', to: 'static_pages#cgv', via: 'get'
+  match '/privacy_policy', to: 'static_pages#privacy_policy', via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
