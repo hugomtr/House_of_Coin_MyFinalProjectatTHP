b045e4b6ef22760ad0a5ab1242b6d9454fd1f346
phbord <phbord@gmail.com>
2021-06-14 11:39:53 +0200

    .env

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+
+puts "*"*30
+puts 'Base de données remplie !'
+puts "*"*30
