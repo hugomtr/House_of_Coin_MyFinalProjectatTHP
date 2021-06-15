440271010169d87392fa308b234877aff1b30844
cedricmesas <mesas.cedric@gmail.com>
2021-06-15 11:11:57 +0200

    admin mailer announcement validation

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -3,4 +3,12 @@ class RealEstate < ApplicationRecord
   friendly_id :name, use: :slugged
   belongs_to :user
   has_many :housecoins
+
+  after_create :real_estate_announce
+
+  private
+  def announce_validation
+    AdminMailer.announce_validation(self).deliver_now
+  end
+
 end
