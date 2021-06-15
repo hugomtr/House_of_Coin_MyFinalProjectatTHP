cfb2f9d8329c6e45f2589c18f4b14efe0515f69b
hugomtr <hugomaitre340@gmail.com>
2021-06-15 17:08:59 +0200

    add geocoder map

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -1,15 +1,27 @@
 class RealEstate < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
+
   belongs_to :user
   has_many :house_coins
 
-  after_create :announce_validation_confirm
+  validates :adress ,presence: true
+  validates :zipcode ,presence: true
+  validates :city ,presence: true
+
+  #after_create :announce_validation_confirm
 
   def coin_price
     self.price / 50 # Arbitrary value for testing
   end
-
+  
+  geocoded_by :full_address
+  after_validation :geocode
+  
+  def full_address
+    "#{adress} ,#{zipcode}, #{city}"
+  end
+  
   private
 
   def announce_validation
