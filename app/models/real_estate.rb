c093af873336db5f5741502f78e62304cdb7f580
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-16 10:21:02 +0200

    Replaced anchor link by link_to in header and uncommented realestate model

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -11,18 +11,18 @@ class RealEstate < ApplicationRecord
   validates :zipcode ,presence: true
   validates :city ,presence: true
 
-  #after_create :announce_validation_confirm
+  after_create :announce_validation_confirm
 
   def price_euros
-  #  price_euros = self.price / 100
+    price_euros = self.price / 100
   end
 
   def coin_number
-  #  house_coins_num = price_euros / 50 # We want our house_coins to cost 50 €
+    house_coins_num = price_euros / 50 # We want our house_coins to cost 50 €
   end
 
   def coin_price
-  #  coin_value = (price_euros / coin_number) * 100 # The returned value must be in cents
+    coin_value = (price_euros / coin_number) * 100 # The returned value must be in cents
   end
   
   geocoded_by :full_address
