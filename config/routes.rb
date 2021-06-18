fb916c535b319fc79b9649d0ca22394e9e4fc2de
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-18 15:23:05 +0200

    Added clear cart button and add one more button to cart

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :real_estates
   resources :charges, only: [:new, :create], :path => 'checkout'
   resources :house_coins, only: [:create, :update, :destroy]
-  resources :carts, only: [:index], :path => 'my_cart'
+  resources :carts, only: [:index, :destroy], :path => 'my_cart'
 
   namespace :admin do
     resources :real_estates
