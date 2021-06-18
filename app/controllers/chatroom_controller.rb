2e3e38d7d8ac8b38d641db9972958adbc1dcb6c3
hugomtr <hugomaitre340@gmail.com>
2021-06-18 16:13:06 +0200

    add messages seed and display messages on chatroom

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,7 +1,7 @@
 class ChatroomController < ApplicationController
   def index
     @real_estate = RealEstate.find(params[:real_estate_id])
-    @investors = find_investors(@real_estate)
+    @messages = find_investors_messages(@real_estate)
   end
 
 end
--- app/helpers/chatroom_helper.rb
+++ app/helpers/chatroom_helper.rb
@@ -1,7 +1,15 @@
 module ChatroomHelper
-  def find_investors(real_estate)
-    unique_users = []
-    unique_users = HouseCoin.where(real_estate_id:real_estate).map{|u| u.user_id}
-    return unique_users
+
+  def find_investors_messages(real_estate)
+    sql = "select distinct *
+          from messages M 
+          where M.user_id in 
+          (select distinct user_id 
+            from house_coins H 
+            where H.real_estate_id=#{real_estate.id})
+            order by created_at"
+    records_array = ActiveRecord::Base.connection.execute(sql)
+    return records_array
   end
+
 end
