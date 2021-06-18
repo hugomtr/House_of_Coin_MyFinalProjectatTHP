24a50cf05a68ebf4dda9708dd9e59019f7a89c4b
hugomtr <hugomaitre340@gmail.com>
2021-06-18 16:50:49 +0200

    litle correction on the chat

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,10 @@
 class ChatroomController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
     @real_estate = RealEstate.find(params[:real_estate_id])
+    @investors  = find_investors(@real_estate)
     @messages = find_investors_messages(@real_estate)
   end
 
