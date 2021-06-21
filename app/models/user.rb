115b9c1ca3bde80573b35934209b5e499da6b71e
cedricmesas <mesas.cedric@gmail.com>
2021-06-21 15:07:48 +0200

    Account confirm controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,24 @@ class User < ApplicationRecord
 
   #TODO reactivate mailers
 
+  # before_create :confirmation_token
   # after_create :welcome_send
 
   private
     def welcome_send
       UserMailer.welcome_email(self).deliver_now
     end
+
+    def confirmation_token
+      if self.confirm_token.blank?
+          self.confirm_token = SecureRandom.urlsafe_base64.to_s
+      end
+    end
+
+    def email_activate
+      self.email_confirmed = true
+      self.confirm_token = nil
+      save!(:validate => false)
+    end
+    
 end
