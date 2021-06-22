a21908207cfb9298acb850df1f15a6ef54416893
phbord <phbord@gmail.com>
2021-06-22 20:39:57 +0200

    update ajax add to cart all functions

--- app/controllers/house_coins_controller.rb
+++ app/controllers/house_coins_controller.rb
@@ -2,30 +2,30 @@ class HouseCoinsController < ApplicationController
     def create
         @order = current_order
         @house_coin = @order.house_coins.new(order_params)
-        @order.save
-        session[:order_id] = @order.id
 
-        respond_to do |format|
-            format.html {
-                redirect_back(fallback_location: root_path)
-            }
-            format.js {}
-        end
-
-        # if @order.save
-        #     session[:order_id] = @order.id
-
-        #     respond_to do |format|
-        #         format.html {
-        #             redirect_back(fallback_location: root_path)
-        #         }
-        #         format.js {}
-        #     end
-        # else
-        #     puts @order.errors.messages
-        #     flash[:notice] = @order.errors.full_messages
-        #     redirect_back(fallback_location: root_path)
+        # @order.save
+        # session[:order_id] = @order.id
+        # respond_to do |format|
+        #     format.html {
+        #         redirect_back(fallback_location: root_path)
+        #     }
+        #     format.js {}
         # end
+
+        if @order.save
+            session[:order_id] = @order.id
+
+            respond_to do |format|
+                format.html {
+                    redirect_back(fallback_location: root_path)
+                }
+                format.js {}
+            end
+        else
+            puts @order.errors.messages
+            flash[:notice] = @order.errors.full_messages
+            redirect_back(fallback_location: root_path)
+        end
     end
 
     def update
@@ -41,7 +41,14 @@ class HouseCoinsController < ApplicationController
         @house_coin.destroy
         @house_coins = @order.house_coins
 
-        redirect_back(fallback_location: root_path)
+        respond_to do |format|
+            format.html do 
+                redirect_back(fallback_location: root_path)
+            end
+
+            format.js do
+            end
+        end
     end
 
     private
