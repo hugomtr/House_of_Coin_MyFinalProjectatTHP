d6f9a48e54ced07c5866a328bc984d1ba4994490
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-23 09:49:06 +0200

    Removed Action cable residual file and folder

--- app/channels/live_discussion_channel.rb
+++
@@ -1,9 +0,0 @@
-# class LiveDiscussionChannel < ApplicationCable::Channel
-#   def subscribed
-#     stream_from "live discussion_channel"
-#   end
-
-#   def unsubscribed
-#     # Any cleanup needed when channel is unsubscribed
-#   end
-# end
