e7d9da9e7441ee44da5100cbaab0ee4acc9c552c
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-25 19:43:06 +0200

    Bugfix homepage controller

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,5 @@
 class HomepageController < ApplicationController
   def index
-    @estates_latest = RealEstate.lastest_estate.max(4)
+    @estates_latest = RealEstate.latest_validated.max(4)
   end
 end
