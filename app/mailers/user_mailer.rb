04852276fdd3ab81bb101f8b8d97aee59da3d696
cedricmesas <mesas.cedric@gmail.com>
2021-06-25 19:22:24 +0200

    update url email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,18 +4,20 @@ class UserMailer < ApplicationMailer
 
     def welcome_email(user)
         @user = user 
-        @url  = 'http://heroku.com/login' 
+        @url  = 'https://lets-buy-with-crypto.herokuapp.com/' 
         mail(to: @user.email, subject: 'Welcome to House of Coin !') 
     end
 
     def offer_validation(user)
-        @user = User.find(user) 
+        @user = User.find(user)
+        @url  = 'https://lets-buy-with-crypto.herokuapp.com/'  
         mail(to: @user.email, subject: 'Validation of your offer')
     end
 
     def buyer_charge_confirmation(user, order)
         @user  = User.find(user)
         @order = Order.find(order)
+        @url  = 'https://lets-buy-with-crypto.herokuapp.com/'
         mail(to: @user.email, subject: 'Confirmation of your order')
     end
 end
